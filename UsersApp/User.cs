59744a2dfb5e20472e220ee9eafa0477e8eaa104
Asya19 <asya.kisko@yandex.ru>
2023-01-02 01:39:10 +0300

    feat: create data base

---
+++ UsersApp/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsersApp
+{
+    class User
+    {
+        private int id { get; set; }
+        private string login { get; set; }
+        private string pass { get; set; }
+        private string email { get; set; }
+
+        public User() { }
+
+        public User(string login, string pass, string email)
+        {
+            this.login = login;
+            this.pass = pass;
+            this.email = email;
+        }
+
+    }
+}
